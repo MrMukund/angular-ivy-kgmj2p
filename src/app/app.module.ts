79274ceacbfbb403ac0e9151ede788ea7fbe153a
MrMukund <csmukund012@gmail.com>
2022-07-22 16:16:24 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TablecontentComponent } from './Table1/tablecontent.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
