614ea933370bbcbe589342c60af266c77b29850f
MrMukund <csmukund012@gmail.com>
2022-07-22 16:23:54 +0530

    1 file modified

--- src/app/Table1/Table1.component.ts
+++ src/app/Table1/Table1.component.ts
@@ -1,34 +1,24 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
   selector: 'tablecontent',
   templateUrl: './Table1.component.html',
   styleUrls: ['./Table1.component.css'],
-
-
-
 })
+export class tablecontentComponent {
+  public headers = ['Name', 'rollno'];
 
-export class tablecontentComponent{
-
-  public headers=['Name', 'rollno']; 
-
-public rows =[
-{
-
-  Name:'Mukund',
-  rollno:'10',
-}
-,
-{
-
-  Name:'Mukund',
-  rollno:'10',
-},
-{
-
-  Name:'Mukund',
-  rollno:'10',
-},
-];
-
+  public rows = [
+    {
+      Name: 'Mukund',
+      rollno: '10',
+    },
+    {
+      Name: 'Mukund',
+      rollno: '10',
+    },
+    {
+      Name: 'Mukund',
+      rollno: '10',
+    },
+  ];
 }
